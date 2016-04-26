41920e3a5d2b995a26967177181833c0bd046907
SiaraQ <siaraq@gmail.com>
2016-04-26 13:19:16 +0200

    Update and rename cibuild.sh to cibuild.bat

---
+++ cibuild.bat
@@ -0,0 +1,11 @@
+# change directory to our repo 
+cd protractor-gherkin-cucumberjs-angular2
+
+# install the repo with npm 
+npm install
+
+# start the server 
+npm start 
+
+# run the tests ( this will both run the E2E & unit tests ) 
+npm run e2e
